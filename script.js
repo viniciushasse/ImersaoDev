98371da71e6545c6baf06427dfb208231682d215
viniciushasse <99216933+viniciushasse@users.noreply.github.com>
2022-04-19 09:41:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var listaFilmes = [
+  "https://br.web.img2.acsta.net/pictures/210/506/21050637_20131017235623573.jpg",
+  "https://br.web.img2.acsta.net/pictures/210/513/21051366_20131021213607144.jpg",
+  "https://br.web.img3.acsta.net/medias/nmedia/18/90/95/96/20122166.jpg"
+];
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src =" + listaFilmes[i] + ">");
+}
